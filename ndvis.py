ec4166b2ff2fdbb5507c3ecbf9dc6f3cf915e1f7
transiteration <miras77756@gmail.com>
2024-06-21 14:07:58 +0900

    Added ndvis.py to make ndvi tiles

--- ndvis.py
+++ ndvis.py
@@ -1,6 +1,6 @@
 import os
 import glob
-import json
+import shutil
 import argparse
 import rasterio
 import subprocess
@@ -8,24 +8,18 @@ import numpy as np
 from osgeo import gdal
 from rasterio.windows import Window
 
-def get_bbox(tiff_path):
-    dataset = gdal.Open(tiff_path)
-    transform = dataset.GetGeoTransform()
-    x_min = transform[0]
-    y_max = transform[3]
-    x_max = x_min + transform[1] * dataset.RasterXSize
-    y_min = y_max + transform[5] * dataset.RasterYSize
-    return y_min, x_min, y_max, x_max
-
 def read_band(band_path):
     dataset = gdal.Open(band_path, gdal.GA_ReadOnly)
     band = dataset.GetRasterBand(1).ReadAsArray()
     return band
 
-def write_band(band, reference_path, output_path):
+def write_band(band, reference_path, output_path, ndvi=False):
     dataset = gdal.Open(reference_path, gdal.GA_ReadOnly)
     driver = gdal.GetDriverByName("GTiff")
-    out_dataset = driver.Create(output_path, dataset.RasterXSize, dataset.RasterYSize, 1, gdal.GDT_Byte)
+    if ndvi:
+        out_dataset = driver.Create(output_path, dataset.RasterXSize, dataset.RasterYSize, 1, gdal.GDT_Float32)
+    else:
+        out_dataset = driver.Create(output_path, dataset.RasterXSize, dataset.RasterYSize, 1, gdal.GDT_Byte)
     out_dataset.SetGeoTransform(dataset.GetGeoTransform())
     out_dataset.SetProjection(dataset.GetProjection())
     out_dataset.GetRasterBand(1).WriteArray(band)
@@ -77,39 +71,34 @@ def crop_black_borders(image_path, cropped_image_path):
         with rasterio.open(cropped_image_path, "w", **meta) as dst:
             dst.write(cropped_image_array)
         return True
-    
-def process_directories(refs_dir, tiles_dir, sentinel_dir, gamma=1.0, agriculture=False):
+
+def process_directories(tiles_dir, sentinel_dir,):
     for safe_dir in os.listdir(sentinel_dir):
         safe_path = os.path.join(sentinel_dir, safe_dir)
         if not os.path.isdir(safe_path):
             continue
-        
-        if agriculture:
-            band_1_path = glob.glob(safe_path + '*/*/*/*/*/*B11_20m.jp2')[0] 
-            band_2_path = glob.glob(safe_path + '*/*/*/*/*/*B08_10m.jp2')[0]
-            band_3_path = glob.glob(safe_path + '*/*/*/*/*/*B02_10m.jp2')[0]
-        else:
-            band_1_path = glob.glob(safe_path + '*/*/*/*/*/*B04_10m.jp2')[0] 
-            band_2_path = glob.glob(safe_path + '*/*/*/*/*/*B03_10m.jp2')[0]
-            band_3_path = glob.glob(safe_path + '*/*/*/*/*/*B02_10m.jp2')[0]           
 
-        band_1 = read_band(band_1_path)
-        band_2 = read_band(band_2_path)
-        band_3 = read_band(band_3_path)
+        nir_path = glob.glob(safe_path + '*/*/*/*/*/*B08_10m.jp2')[0] 
+        red_path = glob.glob(safe_path + '*/*/*/*/*/*B04_10m.jp2')[0]
 
-        band_1_hs = hist_stretching(band_1, gamma=gamma)
-        band_2_hs = hist_stretching(band_2, gamma=gamma)
-        band_3_hs = hist_stretching(band_3, gamma=gamma)
+        refs_dir = "./data/ndvi_ref"
 
-        new_band_1_path = os.path.join(os.path.dirname(band_1_path), os.path.basename(band_1_path).replace('.jp2', '_new_swir.tif'))
-        new_band_2_path = os.path.join(os.path.dirname(band_2_path), os.path.basename(band_2_path).replace('.jp2', '_new_nir.tif'))
-        new_band_3_path = os.path.join(os.path.dirname(band_3_path), os.path.basename(band_3_path).replace('.jp2', '_new_blue.tif'))
+        os.makedirs(refs_dir, exist_ok=True)
+        os.makedirs(tiles_dir, exist_ok=True)
 
-        write_band(band_1_hs, band_1_path, new_band_1_path)
-        write_band(band_2_hs, band_2_path, new_band_2_path)
-        write_band(band_3_hs, band_3_path, new_band_3_path)
+        nir = read_band(nir_path)
+        red = read_band(red_path)
 
-        ref_filename = os.path.basename(band_3_path).replace("_B02_10m.jp2", ".tif")
+        nir_hs = hist_stretching(nir)
+        red_hs = hist_stretching(red)
+
+        new_nir_path = os.path.join(os.path.dirname(nir_path), os.path.basename(nir_path).replace('.jp2', '_new_nir.tif'))
+        new_red_path = os.path.join(os.path.dirname(red_path), os.path.basename(red_path).replace('.jp2', '_new_red.tif'))
+
+        write_band(nir_hs, nir_path, new_nir_path)
+        write_band(red_hs, red_path, new_red_path)
+
+        ref_filename = os.path.basename(red_path).replace("_B04_10m.jp2", ".tif")
         ref_path = os.path.join(refs_dir, ref_filename)
         vrt_path = ref_path.replace(".tif", ".vrt")
         wgs_path = ref_path.replace(".tif", "_wgs84.tif")
@@ -118,9 +107,8 @@ def process_directories(refs_dir, tiles_dir, sentinel_dir, gamma=1.0, agricultur
             "gdalbuildvrt",
             "-separate",
             vrt_path,
-            new_band_1_path,
-            new_band_2_path, 
-            new_band_3_path,
+            new_nir_path,
+            new_red_path, 
         ])
         subprocess.run([
             "gdal_translate",
@@ -135,22 +123,7 @@ def process_directories(refs_dir, tiles_dir, sentinel_dir, gamma=1.0, agricultur
             wgs_path
         ], check=True)
 
-        os.remove(ref_path)
-        os.remove(vrt_path)
-        os.remove(new_band_1_path)
-        os.remove(new_band_2_path)
-        os.remove(new_band_3_path)
         os.rename(wgs_path, ref_path)
-        
-        y_min, x_min, y_max, x_max = get_bbox(ref_path)
-        metadata = {
-            "tif_wgs_output_path": ref_path,
-            "bbox": {"y_min": y_min, "x_min": x_min, "y_max": y_max, "x_max": x_max}
-        }
-        json_path = os.path.join(refs_dir, ref_filename.replace(".tif", ".json"))
-        with open(json_path, "w") as json_file:
-            json.dump(metadata, json_file, indent=4)
-
         tile_output_dir = os.path.join(tiles_dir, ref_filename.split(".")[0])
         os.makedirs(tile_output_dir, exist_ok=True)
         subprocess.run([
@@ -182,24 +155,31 @@ def process_directories(refs_dir, tiles_dir, sentinel_dir, gamma=1.0, agricultur
             new_name = os.path.join(tile_output_dir, f"{os.path.basename(ref_path).replace('.tif', '')}_{idx}.tif")
             os.rename(tile, new_name)
 
+        for filename in os.listdir(tile_output_dir):
+            tile_path = os.path.join(tile_output_dir, filename)
+            ndvi_path = os.path.join(tile_output_dir, filename.replace(".tif", "_ndvi.tif"))
+            dataset = gdal.Open(tile_path, gdal.GA_ReadOnly)
+            b8 = dataset.GetRasterBand(1).ReadAsArray().astype(np.float32)
+            b4 = dataset.GetRasterBand(2).ReadAsArray().astype(np.float32)
+            ndvi = np.zeros_like(b8, dtype=np.float32)
+            valid_mask = (b8 + b4) != 0
+            ndvi[valid_mask] = (b8[valid_mask] - b4[valid_mask]) / (b8[valid_mask] + b4[valid_mask])
+            ndvi = np.nan_to_num(ndvi)
+            ndvi = np.clip(ndvi, -1, 1)
+            write_band(ndvi, tile_path, ndvi_path, ndvi=True)
+            os.remove(tile_path)
+            os.rename(ndvi_path, tile_path)
+        shutil.rmtree(refs_dir)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--data_path", type=str, default="./data", help="Path to create a folder with dataset")
     parser.add_argument("-s", "--sentinel_dir", type=str, default="./sentinel_folder", help="Path to directory with .SAFE folders from Copernicus Browser")
-    parser.add_argument("-g", "--gamma", type=float, default=1.0, help="Gamma value for histogram stretching")
-    parser.add_argument("-a", "--agr", type=bool, default=None, help="Whether to use bands for agriculture monitoring")
-
     args = parser.parse_args()
-
-    refs_dir = os.path.join(args.data_path, "refs/")
-    tiles_dir = os.path.join(args.data_path, "tiles/")
+    tiles_dir = os.path.join(args.data_path, "tiles_ndvi")
 
     os.makedirs(args.data_path, exist_ok=True)
     os.makedirs(tiles_dir, exist_ok=True)
-    os.makedirs(refs_dir, exist_ok=True)
-
-    process_directories(refs_dir=refs_dir,
-                        tiles_dir=tiles_dir,
-                        sentinel_dir=args.sentinel_dir,
-                        gamma=args.gamma,
-                        agriculture=args.agr)
+    
+    process_directories(tiles_dir=tiles_dir,
+                        sentinel_dir=args.sentinel_dir)
